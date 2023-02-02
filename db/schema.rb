3d35cb6d47c8f03363aa58dcab8709eb2ea8d22c
Amanda Smith <ajs.amandasmith@gmail.com>
2023-02-02 16:43:08 -0600

    Create Group-User join table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_224038) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_224210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_224038) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
