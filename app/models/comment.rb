9f06e7e509df22424cbc272df3ab133c63891e88
Amanda Smith <ajs.amandasmith@gmail.com>
2023-02-27 16:14:36 -0600

    Add Tag and Post_Tag Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :chracter
+  belongs_to :character
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :chracter
+  belongs_to :character
   belongs_to :user
   has_many :comments
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
