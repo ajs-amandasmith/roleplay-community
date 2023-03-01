fac7059174dd5e8732c987b21a2747cfa66a7bc0
Amanda Smith <ajs.amandasmith@gmail.com>
2023-03-01 15:06:14 -0600

    Add Tailwind CSS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   skip_before_action :authorize
 
   def index
-    render json: Post.all, include: :tags
+    render json: Post.all, include: [:tags, :comments]
   end
 end
