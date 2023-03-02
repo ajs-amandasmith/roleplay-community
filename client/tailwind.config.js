f7fcf10b4bf8af96a11279011f4c468a8b51a8d6
Amanda Smith <ajs.amandasmith@gmail.com>
2023-03-02 14:27:47 -0600

    Add Tailwind testing and README addition

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    extend: {
+      colors: {
+        vuejs: "#49e659"
+      },
+    },
   },
   plugins: [],
 }
