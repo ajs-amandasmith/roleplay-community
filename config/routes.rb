e9f7eccc62dacc9e938510a2f6487c9308505b18
Amanda Smith <ajs.amandasmith@gmail.com>
2023-03-02 15:29:33 -0600

    Create SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
 
+  # sessions
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
