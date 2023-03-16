505019e18cb552486d4d63890d7688fc995726a0
Amanda Smith <ajs.amandasmith@gmail.com>
2023-03-16 14:38:23 -0500

    Add features to adding/updating a user's avatar

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :username, :about_me, :avatar
+  attributes :id, :username, :about_me, :avatar, :posts
+
+  has_many :posts
+  has_many :characters
+  has_many :comments
 
   def avatar
     rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
--- client/src/Components/ProfilePage.js
+++ client/src/Components/ProfilePage.js
@@ -1,18 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../Context/user";
 import AddAvatarForm from "./AddAvatarForm";
 
 function ProfilePage() {
   const { user } = useContext(UserContext);
+  const [addAvatar, setAddAvatar] = useState(false);
 
-  console.log('user', user)
+  console.log('user', user.comments)
+
+  function handleAddAvatar() {
+    setAddAvatar(!addAvatar)
+  }
 
   return (
     <div>
       <h1 className="mt-1 text-5xl font-medium leading-tight">Welcome, {user.username}!</h1>
       <h2 className="mt-1 text-4xl">You can update your profile settings here!</h2>
-      <img className="h=[300px] w-[300px] object-cover" src={user?.avatar} />
-      <AddAvatarForm />
+      <img className="h=[100px] w-[100px] object-cover" src={user?.avatar} />
+      {addAvatar ? 
+        <button className="border" onClick={handleAddAvatar}>Cancel</button> :
+        <button className="border" onClick={handleAddAvatar}>{user.avatar ? "Update Avatar?" : "Add Avatar?"}</button>
+      }
+      {addAvatar ? <AddAvatarForm /> : null}
     </div>
   )
 }
