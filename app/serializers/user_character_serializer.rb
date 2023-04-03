dc67e7c6946c70958d751e8a33b47af0a18789da
Amanda Smith <ajs.amandasmith@gmail.com>
2023-04-03 14:08:23 -0500

    Allow a user to update a character avatar

--- app/serializers/user_character_serializer.rb
+++ app/serializers/user_character_serializer.rb
@@ -1,4 +1,9 @@
 class UserCharacterSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :name, :avatar, :about
+
+  def avatar
+    rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
+  end
 
 end
