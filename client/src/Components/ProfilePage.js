d690366cf85c8bfd3df4c98673bb49af8ebc9fcf
Amanda Smith <ajs.amandasmith@gmail.com>
2023-04-05 19:50:20 -0500

    Show default image if user doesn't have an avatar

--- client/src/Components/ProfilePage.js
+++ client/src/Components/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import AddAvatarForm from "./AddAvatarForm";
+import blank_avatar from '../images/blank_avatar.png';
 
 function ProfilePage() {
   const state = useSelector(state => state)
@@ -15,15 +16,6 @@ function ProfilePage() {
     </div>
   ))
 
-  console.log('state', state)
-  console.log('user', user)
-  console.log('status', status);
-  console.log('characters', characters)
-
-  // console.log('comments', user.comments)
-  // console.log('posts', user.posts)
-  
-
   function handleAddAvatar() {
     setAddAvatar(!addAvatar)
   }
@@ -34,7 +26,7 @@ function ProfilePage() {
       <div>
         <h1 className="mt-1 text-5xl font-medium leading-tight">Welcome, {user.username}!</h1>
         <h2 className="mt-1 text-4xl">You can update your profile settings here!</h2>
-        <img className="h=[100px] w-[100px] object-cover" src={user?.avatar} alt='user profile' />
+        <img className="h=[100px] w-[100px] object-cover" src={typeof user.avatar === "string" ? user.avatar : blank_avatar} alt='user profile' />
         {addAvatar ? 
           <button className="border" onClick={handleAddAvatar}>Cancel</button> :
           <button className="border" onClick={handleAddAvatar}>{user.avatar ? "Update Avatar?" : "Add Avatar?"}</button>
