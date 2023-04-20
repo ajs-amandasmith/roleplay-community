ee8f134eda2272214c60ab87b76edbcfb9dcb29b
Amanda Smith <ajs.amandasmith@gmail.com>
2023-04-20 12:00:47 -0500

    Show an error when a file isn't attached when updating a user's avatar

--- client/src/Components/AddAvatarForm.js
+++ client/src/Components/AddAvatarForm.js
@@ -2,32 +2,35 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function AddAvatarForm({ addAvatar, setAddAvatar }) {
-  const [avatar, setAvatar] = useState();
-  // const [errors, setErrors] = useState();
+  const [avatar, setAvatar] = useState("");
+  const [error, setError] = useState("");
   const user = useSelector(state => state);
   const dispatch = useDispatch();
+  const [noAvatar, setNoAvatar] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('avatar', avatar);
-    fetch(`/users/${user.id}`, {
-      method: "PATCH",
-      body: formData
-    })
-      .then((r) => {
-        if (r.ok) {
-          // r.json().then(data => setUser(data))
-          r.json().then(data => {
-            console.log('data', data)
-            console.log('formData', formData)
-            dispatch({ type: 'user/avatar/update', payload: data.avatar})
-            setAddAvatar(!addAvatar)
-          })
-        } else {
-          // r.json().then((err) => setErrors(err.errors))
-        }
+    setNoAvatar(false);
+    if (avatar === "" ) {
+      setNoAvatar(true);
+    } else {
+      const formData = new FormData();
+      formData.append('avatar', avatar);
+      fetch(`/users/${user.id}`, {
+        method: "PATCH",
+        body: formData
       })
+        .then((r) => {
+          if (r.ok) {
+            r.json().then(data => {
+              dispatch({ type: 'user/avatar/update', payload: data.avatar})
+              setAddAvatar(!addAvatar)
+            })
+          } else {
+            r.json().then((err) => setError(err.error))
+          }
+        })
+    }
   }
 
   return (
@@ -37,6 +40,8 @@ function AddAvatarForm({ addAvatar, setAddAvatar }) {
         <input id="avatar" type="file" accept="image" className="border" onChange={e => setAvatar(e.target.files[0])} ></input>
         <button type="submit" className="btn-confirm">Submit Image</button>
       </form>
+      {noAvatar ? <p className="text-rose-500">No file attached</p> : null}
+      {error === "Internal Server Error" ? <p className="text-rose-500">No file attached</p> : null }
     </div>
   )
 }
